482167408e31ed6f9cb309bd4a7794d3ebe3e734
aashumakova <aashumakova@gmail.com>
2023-06-05 11:54:00 -0700

    details, index page, banner work on with habib, matt, neelu

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('children/', views.children_index, name='index'),
+    path('children/<int:child_id>/', views.children_detail, name='detail'),
+    
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Child
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def children_index(request):
+    children = Child.objects.filter(user=request.user)
+    return render(request, 'children/index.html', {
+        'children': children
+    })
+
+def children_detail(request, child_id):
+    child = Child.object.get(id=child_id)
+    return render(request, 'children/detail.html', {
+        'child': child
+    })
