3d1596a6a0de744e258324c8b56884838c88b6ff
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-06-05 16:32:22 -0400

    ini

--- bookreading/settings.py
+++ bookreading/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'habibpsql',
+        'HOST': 'localhost',
+        'PORT': 5000,
     }
 }
 
