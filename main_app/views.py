da9685e360728650c1cb43cff244d7ade3b49f20
mbalzano19 <125160521+mbalzano19@users.noreply.github.com>
2023-06-05 17:43:35 -0400

    adds login authentication, child create form, as well as edit and delete buttons for a created child. worked on with whole team - matt, alina, neelu, abib

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Child
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -12,12 +16,14 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def children_index(request):
     children = Child.objects.all()
     return render(request, 'children/index.html', {
         'children': children
     })
 
+@login_required
 def children_detail(request, child_id):
     child = Child.objects.get(id=child_id)
     return render(request, 'children/detail.html', {
@@ -26,8 +32,12 @@ def children_detail(request, child_id):
 
 class ChildCreate(CreateView):
     model = Child
-    fields = '__all__'
+    fields = ['name', 'age_group']
     success_url = '/children'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class ChildUpdate(UpdateView):
     model = Child
@@ -36,3 +46,17 @@ class ChildUpdate(UpdateView):
 class ChildDelete(DeleteView):
     model = Child
     success_url = '/children'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again. Go home.'
+    form = UserCreationForm()
+    context = { 'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
