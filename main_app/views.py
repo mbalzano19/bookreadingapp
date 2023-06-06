3933d8bf7285e5daf27efd2c2cc31b2eb066c32d
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-06-06 09:18:37 -0400

    ini

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Child
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .form import ChildForm
 
 # Create your views here.
 
@@ -20,8 +21,10 @@ def children_index(request):
 
 def children_detail(request, child_id):
     child = Child.objects.get(id=child_id)
+    childform = ChildForm()
     return render(request, 'children/detail.html', {
         'child': child,
+        'childform':childform
     })
 
 class ChildCreate(CreateView):
@@ -36,3 +39,4 @@ class ChildUpdate(UpdateView):
 class ChildDelete(DeleteView):
     model = Child
     success_url = '/children'
+    
