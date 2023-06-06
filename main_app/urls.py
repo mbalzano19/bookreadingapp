7e72b39cec314e3761d14bf8d272ac1549b224ae
NeeluT <nilufar.tavakoli@gmail.com>
2023-06-06 17:28:08 -0400

    worked on adding books to children profiles with Habib, Alina, Matt

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,13 +4,19 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('books/', views.books_index, name='books_index'),
-    path('books/<str:book_title>/', views.books_detail, name='books_detail'),
+    # path('books/<str:book_title>/', views.books_detail, name='books_detail'),
     path('children/', views.children_index, name='index'),
     path('children/<int:child_id>/', views.children_detail, name='detail'),
     path('children/create/', views.ChildCreate.as_view(), name='child_create'),
+    # path('children/<int:user_id>/add_child', views.add_child, name='add_child'),
+    path('children/<int:child_id>/assoc_book/<int:book_id>', views.assoc_book, name='assoc_book'),
+    path('children/<int:child_id>/disassoc_book/<int:book_id>', views.disassoc_book, name='disassoc_book'),
     path('children/<int:pk>/update/', views.ChildUpdate.as_view(), name='child_update'),
     path('children/<int:pk>/delete/', views.ChildDelete.as_view(), name='child_delete'),
+    path('books/', views.books_index, name='books_index'),
+    path('books/', views.BookList.as_view(), name='books_index'),
+    path('books/<int:pk>/', views.BookDetail.as_view(), name='books_detail'),
     path('accounts/singup', views.signup, name='signup'),
+    
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Child
+from .models import Child, Book
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .data import data
+# from .data import data
+from .forms import ChildForm
 
 
 # Create your views here.
-data = data()
+# data = data()
 
 def home(request):
     return render(request, 'home.html')
@@ -19,18 +20,12 @@ def about(request):
     return render(request, 'about.html')
 
 def books_index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html',{'books': books})
 
-    return render(request, 'books/index.html',{'books':data})
-
-def books_detail(request,book_title):
-    for book in data:
-        print(book)
-        print(book.get('title'))
-        print(book_title)
-        if(book.get('title')==book_title):
-            v=book
-    
-    return render(request, 'books/detail.html',{'book':v})
+def books_detail(request,book_id):
+    books = Book.objects.filter(id=book_id)
+    return render(request, 'books/detail.html',{'book': books})
 
 
 @login_required
@@ -40,14 +35,27 @@ def children_index(request):
         'children': children
     })
 
+# @login_required
+# def children_detail(request, child_id):
+#     child = Child.objects.get(id=child_id)
+#     books = Book.objects.filter(user=request.user)
+#     return render(request, 'children/detail.html', {
+#         'child': child,
+#         'books': books
+#     })
+
 @login_required
 def children_detail(request, child_id):
     child = Child.objects.get(id=child_id)
-    childform = ChildForm()
+    id_list = child.books.all().values_list('id')
+    books_child_doesnt_have = Book.objects.exclude(id__in=id_list)
     return render(request, 'children/detail.html', {
         'child': child,
-        'childform':childform
+        'books': books_child_doesnt_have
     })
+
+
+
 
 class ChildCreate(CreateView):
     model = Child
@@ -58,6 +66,15 @@ class ChildCreate(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+# def add_child(request, user_id):
+#   form = ChildForm(request.POST)
+#   if form.is_valid():
+#     new_child = form.save(commit=False)
+#     new_child.user_id = user_id
+#     new_child.save()
+#   return redirect('detail', user_id=user_id)
+
+
 class ChildUpdate(UpdateView):
     model = Child
     fields = ['age_group']
@@ -66,7 +83,24 @@ class ChildDelete(DeleteView):
     model = Child
     success_url = '/children'
 
+@login_required
+def assoc_book(request,child_id, book_id):
+    Child.objects.get(id=child_id).books.add(book_id)
+    return redirect('detail', child_id=child_id)
+
+@login_required
+def disassoc_book(request, child_id, book_id):
+    Child.objects.get(id=child_id).books.remove(book_id)
+    return redirect('detail', child_id=child_id)
+
+class BookList(LoginRequiredMixin,ListView):
+  model = Book
+
+class BookDetail(LoginRequiredMixin,DetailView):
+  model = Book
+
 def signup(request):
+    print(request)
     error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
